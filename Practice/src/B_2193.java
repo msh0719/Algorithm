0bfbb6925fffec92cd617bc4cdfa3e2a3f0800c9
명수현 <myeong@myeongsuhyeon-ui-MacBook-Pro.local>
2018-02-12 17:22:13 +0900

    no message

---
+++ Practice/src/B_2193.java
@@ -0,0 +1,30 @@
+import java.io.*;
+/*
+이친수
+ */
+
+public class B_2193 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[91];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3; i<91; i++){
+            dp[i] = dp[i-1] + dp[i-2]; // -1, -2의 이친수들이 i 이친수에 부분이 된다.
+        }
+
+        bw.write(dp[N] + "\n");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
